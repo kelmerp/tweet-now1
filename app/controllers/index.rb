ac1228d5e21551b5c1aa83ea7487b0947447b9e1
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-20 17:28:10 -0500

    mvp except for success check

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,5 +4,10 @@ end
 
 post '/send' do
   tweet = params["body"]
+  puts tweet
   
+  @success = CLIENT.update(tweet)
+  p @success
+  
+  erb :index
 end
--- public/js/application.js
+++ public/js/application.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $("form.tweet").on("submit", function(event){
     event.preventDefault();
     form = $(this).serialize();
-    $.post('/send', form, function(success) {
-      console.log(success);
+    $.post('/send', form, function(message){
+      $(this).remove();
+      $('body').append(message);
     });
   });
 });
